a82433f8825de119cfa1cec0e07249879b73a0ef
quadrismegistus <ryan.heuser@gmail.com>
2020-09-29 13:43:36 +0100

    updates

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ with open('content.html') as content_f,open('index.html','w') as of:
     content = content_f.read() #.replace('\n  * ','</li>\n<li>')
     content=content.replace('&lt;','<').replace('&gt;','>')
     content=content.replace('komrade/app/assets','assets')
-    content=content.replace('<h1 id="komrade">Comrad</h1>','')
+    content=content.replace('<h1 id="comrad">Comrad</h1>','')
     content=content.replace('href="komrade/','href="https://github.com/Comrad/Comrad/tree/master/komrade/')
     total = theme.replace('[[CONTENT]]',content)
     of.write(total)
